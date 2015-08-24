e1a1afabac73fc463cb98d47ea5ba87ad6c28535
shaunfreeman <shaun@shaunfreeman.co.uk>
2015-08-24 21:58:13 +0100

    admin menu update

--- config/module.config.php
+++ config/module.config.php
@@ -193,28 +193,24 @@ return [
     ],
     'navigation' => [
         'admin' => [
-            'modules' => [
+            'business-list' => [
+                'label' => 'Business List',
                 'pages' => [
-                    'business-list' => [
-                        'label' => 'Business List',
-                        'pages' => [
-                            'list' => [
-                                'label'     => 'List All Businesses',
-                                'action'    => 'list',
-                                'route'     => 'admin/business-list',
-                                'resource'  => 'menu:user'
-                            ],
-                            'add' => [
-                                'label'     => 'Add New Business',
-                                'action'    => 'add',
-                                'route'     => 'admin/business-list/edit',
-                                'resource'  => 'menu:user'
-                            ],
-                        ],
-                        'route' => 'admin/business-list',
-                        'resource' => 'menu:user'
+                    'list' => [
+                        'label'     => 'List All Businesses',
+                        'action'    => 'list',
+                        'route'     => 'admin/business-list',
+                        'resource'  => 'menu:user'
+                    ],
+                    'add' => [
+                        'label'     => 'Add New Business',
+                        'action'    => 'add',
+                        'route'     => 'admin/business-list/edit',
+                        'resource'  => 'menu:user'
                     ],
                 ],
+                'route' => 'admin/business-list',
+                'resource' => 'menu:user'
             ],
         ],
     ],
